20c525ade41921fa6a5007a923f4b679eb38d520
Keerthanaswamynathan <sk.keerthu93@gmail.com>
2021-08-24 07:28:59 -0700

    my path

---
+++ src/org/sam/SampleProg.java
@@ -0,0 +1,31 @@
+package org.sam;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SampleProg {
+	public static void main(String[] args) throws InterruptedException {
+	WebDriver driver;
+	System.setProperty("webdriver.chrome.driver",
+			"C:\\Users\\Mythili\\eclipse-workspace\\GaneshGod\\driver\\chromedriver.exe");
+	driver = new ChromeDriver();
+	driver.get("https://www.amazon.com/");
+	WebElement search = driver.findElement(By.xpath("(//input[@type='text'])"));
+	//search.click();
+	search.sendKeys("iphone");
+	WebElement searchbtn = driver.findElement(By.xpath("(//input[@type='submit'])"));
+	searchbtn.click();
+	WebElement selecttitle = driver.findElement(By.xpath("//a[@class='a-link-normal a-text-normal']//child::span[contains(text(),'TracFone')]"));
+	Thread.sleep(3000);
+	selecttitle.click();
+	String printtitle = driver.findElement(By.xpath("//span[contains(text(),'TracFone i')]//parent::h1")).getText();
+	System.out.println(printtitle);
+	String printprice = driver.findElement(By.xpath("//span[@id='price_inside_buybox']//parent::div")).getText();
+	System.out.println(printprice);
+	String deleviery = driver.findElement(By.xpath("//div[@class='a-spacing-base']//child::b")).getText();
+	System.out.println(deleviery);
+	}
+	}
+
